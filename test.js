1ee1b74a3209b469c8a96268ec0ab7d2624d0f54
songtao <songtao_st08@163.com>
2019-04-08 11:24:25 +0800

    修改testJS

--- test.js
+++ test.js
@@ -3,4 +3,12 @@ const test = (type) => (age) => {
     console.log(age);
 }
 
-test('1')(26)
+const testTwo = (type) => {
+    return (age) => {
+        console.log(type);
+        console.log(age);
+    }
+}
+
+test('1')(26);
+testTwo('2')(27);
